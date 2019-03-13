eecd06fdc3054f1c30e96ea71d48d7764fda933e
ElVini <2016030004@upsin.edu.mx>
2019-03-13 07:58:04 -0700

    Me equivoqué de base de datos :(

--- bd.sql
+++ bd.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.5.5-10.1.29-MariaDB : Database - chongues
+MySQL - 5.5.5-10.1.29-MariaDB : Database - consultorio
 *********************************************************************
 */
 
@@ -12,9 +12,29 @@ MySQL - 5.5.5-10.1.29-MariaDB : Database - chongues
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`chongues` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`consultorio` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 
-USE `chongues`;
+USE `consultorio`;
+
+/*Table structure for table `citas` */
+
+DROP TABLE IF EXISTS `citas`;
+
+CREATE TABLE `citas` (
+  `idConsultas` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `diaConsulta` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `horaConsulta` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombrePaciente` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellidoPaciente` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`idConsultas`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `citas` */
+
+insert  into `citas`(`idConsultas`,`diaConsulta`,`horaConsulta`,`nombrePaciente`,`apellidoPaciente`,`fechaNacimiento`,`created_at`,`updated_at`) values (1,'Lunes','12:00 pm - 01:00 pm','Vinicio','Martínez','1998-11-30',NULL,'2019-03-13 04:32:03'),(3,'Lunes','01:00 pm - 02:00 pm','Marcos','Joto','1997-08-02','2019-03-07 15:59:34','2019-03-12 16:26:59'),(4,'Miércoles','02:00 pm - 03:00 pm','Erick','Medina','1998-04-03','2019-03-07 16:00:05','2019-03-07 16:00:05'),(5,'Jueves','04:00 pm - 05:00 pm','Vinicio','Martínez','1998-11-30','2019-03-13 04:37:21','2019-03-13 04:37:21'),(6,'Miércoles','11:00 am - 12:00 pm','asdasd','alskdasd','2001-08-13','2019-03-13 04:38:22','2019-03-13 04:38:22');
 
 /*Table structure for table `migrations` */
 
@@ -25,98 +45,11 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `migrations` */
 
-insert  into `migrations`(`id`,`migration`,`batch`) values (18,'2014_10_12_000000_create_users_table',1),(19,'2014_10_12_100000_create_password_resets_table',1),(20,'2019_03_03_043946_create_ventas_table',1),(21,'2019_03_06_165839_productos',1),(22,'2019_03_07_201335_venta_has_productos',1);
-
-/*Table structure for table `password_resets` */
-
-DROP TABLE IF EXISTS `password_resets`;
-
-CREATE TABLE `password_resets` (
-  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  KEY `password_resets_email_index` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-/*Data for the table `password_resets` */
-
-/*Table structure for table `productos` */
-
-DROP TABLE IF EXISTS `productos`;
-
-CREATE TABLE `productos` (
-  `idProductos` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `codigo` int(10) unsigned NOT NULL,
-  `descripcion` varchar(150) COLLATE utf8_bin NOT NULL,
-  `precio` double(8,2) NOT NULL,
-  `stock` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`idProductos`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `productos` */
-
-insert  into `productos`(`idProductos`,`codigo`,`descripcion`,`precio`,`stock`,`created_at`,`updated_at`) values (2,92,'Gorra shida',89.99,10,NULL,NULL),(3,93,'Camisa',134.99,10,NULL,NULL);
-
-/*Table structure for table `users` */
-
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(191) COLLATE utf8_bin NOT NULL,
-  `email` varchar(191) COLLATE utf8_bin NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(191) COLLATE utf8_bin NOT NULL,
-  `tipoUsuario` int(10) unsigned NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `users` */
-
-insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`tipoUsuario`,`remember_token`,`created_at`,`updated_at`) values (2,'admin','admin',NULL,'$2y$10$9nNo/8glcuBKT1HEI2FOmenY2qKqXgd9dLJbIbfC4avY1G0bDNDZG',1,NULL,NULL,NULL);
-
-/*Table structure for table `venta_has_productos` */
-
-DROP TABLE IF EXISTS `venta_has_productos`;
-
-CREATE TABLE `venta_has_productos` (
-  `idVenta` int(10) unsigned NOT NULL,
-  `idProducto` int(10) unsigned NOT NULL,
-  `articulos` int(10) unsigned NOT NULL,
-  KEY `venta_has_productos_idventa_foreign` (`idVenta`),
-  KEY `venta_has_productos_idproducto_foreign` (`idProducto`),
-  CONSTRAINT `venta_has_productos_idproducto_foreign` FOREIGN KEY (`idProducto`) REFERENCES `productos` (`idProductos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `venta_has_productos_idventa_foreign` FOREIGN KEY (`idVenta`) REFERENCES `ventas` (`idVentas`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `venta_has_productos` */
-
-/*Table structure for table `ventas` */
-
-DROP TABLE IF EXISTS `ventas`;
-
-CREATE TABLE `ventas` (
-  `idVentas` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idUsuario` int(10) unsigned NOT NULL,
-  `total` double(8,2) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`idVentas`),
-  KEY `ventas_idusuario_foreign` (`idUsuario`),
-  CONSTRAINT `ventas_idusuario_foreign` FOREIGN KEY (`idUsuario`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `ventas` */
+insert  into `migrations`(`id`,`migration`,`batch`) values (1,'2014_10_12_000000_create_users_table',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
